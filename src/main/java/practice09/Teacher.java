dfd0cdaa6ec002d877d76ff2940664e29faf9c35
xion <313183373@qq.com>
2019-03-27 23:50:30 +0800

    add a students:ArrayList<Student> field in Klass and add a hasStudent method

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,36 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass = null;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    private boolean isKlassNull() {
+        return klass == null;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (isKlassNull() ? "No Class." : (getKlass().getDisplayName() + "."));
+    }
+
+    private boolean isTeaching(Student student) {
+        return student.getKlass().equals(getKlass());
+    }
+
+    String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. I " + (isTeaching(student) ? "" : "don't ") + "teach " + student.getName() + ".";
+    }
 }
+
