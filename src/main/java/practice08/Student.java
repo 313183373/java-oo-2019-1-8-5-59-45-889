c18a41fafa42aab7edc21baddb0eb78de06e5bbc
xion <313183373@qq.com>
2019-03-27 23:26:10 +0800

    add isLeader method and change introduce method

--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,24 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+
+    private final Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am " + (isLeader() ? "Leader of " : "at ") + getKlass().getDisplayName() + ".";
+    }
+
+    public boolean isLeader() {
+        return getKlass().hasLeader() && equals(getKlass().getLeader());
+    }
 }
