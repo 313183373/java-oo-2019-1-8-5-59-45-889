09fb86f41829c0bd654b0b4cc3f70020a349e531
xion <313183373@qq.com>
2019-03-27 22:42:42 +0800

    Klass add a field leader

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,17 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    String getDisplayName() {
+        return "Class " + getNumber();
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass = null;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    private boolean isKlassNull() {
+        return klass == null;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (isKlassNull() ? "No Class." : (getKlass().getDisplayName() + "."));
+    }
+
+    private boolean isTeaching(Student student) {
+        return student.getKlass().getNumber() == getKlass().getNumber();
+    }
+
+    String introduceWith(Student student) {
+        return super.introduce() + " I am a Teacher. I " + (isTeaching(student) ? "" : "don't ") + "teach " + student.getName() + ".";
+    }
 }
+
