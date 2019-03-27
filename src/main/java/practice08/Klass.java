a3d931cdae37330f37aa5ac0057bafccf9beefcb
xion <313183373@qq.com>
2019-03-27 23:25:41 +0800

    add leader field in Klass and assignLeader method and override equals method

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,35 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader = null;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    void assignLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    boolean hasLeader() {
+        return getLeader() != null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj) || ((obj instanceof Klass) && (((Klass) obj).getNumber() == getNumber()));
+    }
 }
