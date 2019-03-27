ea2a911a458a4553a2bb88d7112fdef039729ebc
xion <313183373@qq.com>
2019-03-27 22:10:53 +0800

    add Teacher class and override introduce method

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass = null;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    private boolean isKlassNull() {
+        return klass == null;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + (isKlassNull() ? "No Class." : ("Class " + getKlass() + "."));
+    }
 }
